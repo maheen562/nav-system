9e9dd80bebf08c2dff9df03ba44af41c7318dbad
maheen562 <maheenkhan193@gmail.com>
2026-02-14 16:47:17 +1100

    Added Navbar to App

--- src/App.js
+++ src/App.js
@@ -4,22 +4,9 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <header> </header>
       <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,33 @@
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+/**
+ * The component that renders the navigation bar for the application. 
+ * It uses the react-pro-sidebar library to create
+ * a sidebar with menu items that link to different routes in the application.
+ * The NavLink component from react-router-dom is used to create links that can 
+ * be styled based on the active route.
+ * @returns {JSX.Element} The rendered Navbar component.
+ */
+
+import { Sidebar, Menu, MenuItem} from 'react-pro-sidebar';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
+
   return (
-      <Sidebar>
-        <Menu>
-          <SubMenu label="Charts">
-            <MenuItem> Pie charts </MenuItem>
-            <MenuItem> Line charts </MenuItem>
-          </SubMenu>
-          <MenuItem> Documentation </MenuItem>
-          <MenuItem> Calendar </MenuItem>
+      <Sidebar style={{ height: '100vh' }} collapsed={false}>
+        <Menu
+        menuItemStyles={{
+          button: {
+            [`&.active`]: {
+              backgroundColor: '#13395e',
+              color: '#b6c8d9',
+            },
+          },
+        }}
+        >
+          <MenuItem component={<NavLink to="/employeedirectory" />}
+          active={window.location.pathname === '/employeedirectory'}>Employee Directory </MenuItem>
+          <MenuItem component={<NavLink to="/dashboard" />}
+          active={window.location.pathname === '/dashboard'}> Dashboard  </MenuItem>
         </Menu>
       </Sidebar>
   );
