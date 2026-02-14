b0b9a947f8f4ca9b84cbddae99e76cf338248173
maheen562 <maheenkhan193@gmail.com>
2026-02-14 13:37:37 +1100

    (feat): Added Navbar, using library react-pro-sidebar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+export default function Navbar() {
+  return (
+      <Sidebar>
+        <Menu>
+          <SubMenu label="Charts">
+            <MenuItem> Pie charts </MenuItem>
+            <MenuItem> Line charts </MenuItem>
+          </SubMenu>
+          <MenuItem> Documentation </MenuItem>
+          <MenuItem> Calendar </MenuItem>
+        </Menu>
+      </Sidebar>
+  );
+}
