43dd5916c2b9e4c7f5a2e75e274b2efe698afa26
maheen562 <maheenkhan193@gmail.com>
2026-02-15 12:32:08 +1100

    Added dummy Data to EmployeeDirectory

--- src/components/EmployeeDirectory.jsx
+++ src/components/EmployeeDirectory.jsx
@@ -1,11 +1,23 @@
-
+import React, { use, useEffect, useState } from 'react';
 
 export default function EmployeeDirectory() {
 
-  return (
-    <>
-    <h1> Employee Directory </h1>
+    const [employee, setEmployee] = useState([]);
 
-    </>
+    useEffect(() => {
+        fetch("https://dummyjson.com/c/092c-8ec9-4627-9bb4")
+        .then(res => res.json())
+        .then(data => setEmployee(data));
+    }, []);
+    return (
+        <>
+        <h1> Employee Directory </h1>
+        {employee.map(emp => (
+            <div key={emp.id}>
+            <p>{emp.name} - Department: {emp.department}</p>
+            </div>
+        ))}
+
+        </>
     
 )}
